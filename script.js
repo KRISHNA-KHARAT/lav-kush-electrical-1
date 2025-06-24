b891f9ca82aaae815a4e01e104495e7999d2f52c
KRISHNA-KHARAT <krishnakharat2001@gmail.com>
2025-06-25 04:54:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Simple message in console
+console.log("Website loaded successfully!");
+
+
+
+
+
+
+
+// Fade-in animation on scroll
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("visible");
+    }
+  });
+}, { threshold: 0.2 });
+
+sections.forEach(section => {
+  observer.observe(section);
+});
